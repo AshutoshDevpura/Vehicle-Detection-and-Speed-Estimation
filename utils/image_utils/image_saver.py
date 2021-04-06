dad3e1f10d31cda8bc6567e941b061a321dc8d1e
AshutoshDevpura <46817661+AshutoshDevpura@users.noreply.github.com>
2021-04-06 17:01:44 +0530

    Added Image crop & saver files

---
+++ utils/image_utils/image_saver.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------
+# --- Author         : Ashutosh Devpura
+# --- Mail           : ashutoshdevpura@gmail.com
+#
+# ----------------------------------------------
+
+import cv2
+import os
+
+vehicle_count = [0]
+current_path = os.getcwd()
+
+
+def save_image(source_image):
+    cv2.imwrite(current_path + '/detected_vehicles/vehicle'
+                + str(len(vehicle_count)) + '.png', source_image)
+    vehicle_count.insert(0, 1)
+
+
+
+			
